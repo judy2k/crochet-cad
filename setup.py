8a34dd1e52f6cdd1083935d782ecbda4c5517c1b
Mark Smith <mark.smith@practicalpoetry.co.uk>
2011-04-04 18:26:59 +0100

    Added a setup.py for crochet-cad.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='Crochet CAD',
+    version='0.0.1dev',
+    packages=['crocad',],
+    license='GNU General Public License',
+    description=
+    'A collection of utilities to aid in designing circular crochet patterns, such as Amigurumi.',
+    long_description=''.join(open('README.rst').readlines()[2:]),
+    
+    author='Mark Smith',
+    author_email='mark.smith@practicalpoetry.co.uk',
+    url='https://github.com/bedmondmark/crochet-cad',
+)
