9d62c344681fef422506e140368cd0a9bd5a497e
Mark Smith <mark.smith@practicalpoetry.co.uk>
2012-01-23 11:05:10 +0000

    Added jasmine test scaffold.

--- web/coffee/index.coffee
+++ web/coffee/index.coffee
@@ -1,2 +1,1 @@
-paper = Raphael('demo', 480, 640)
-paper.donut(10, 10, 260, 620, 20, 5)
+
---
+++ web/test/spec/crocad.spec.js
@@ -0,0 +1,10 @@
+describe('crocad', function() {
+    var cc;
+
+    beforeEach(function() {
+        cc = new Crocad();
+    });
+    it('Exists in the ', function() {
+        expect(cc).not.toBeUndefined();
+    });
+});
