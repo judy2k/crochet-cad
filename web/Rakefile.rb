9d5ace23a64cd1fa2316cb823792ec011619933e
Mark Smith <mark.smith@practicalpoetry.co.uk>
2012-01-26 12:50:38 +0000

    Reorganisation complete.
    Sources are now contained in ./src and compiled/transferred to ./dev

--- web/Rakefile.rb
+++ web/Rakefile.rb
@@ -2,7 +2,7 @@ require 'rake/clean'
 
 # Digest::MD5.file('Vagrantfile').hexdigest[0...8]
 
-task :default => 'dev/index.html'
+task :default => :devel
 
 directory "dev"
 CLOBBER.include 'dev'
@@ -10,9 +10,40 @@ CLOBBER.include 'dev'
 file 'dev/index.html' => ['dev', 'src/index.html'] do
     cp 'src/index.html', 'dev/index.html'
 end
+task :devel => 'dev/index.html'
 CLOBBER.include 'dev/index.html'
 
-for dep in FileList['src/js/lib/*.js']
+directory "dev/js/lib"
+CLOBBER.include 'dev/js/lib'
+
+FileList['src/js/lib/*.js'].each do |src|
+    target = src.pathmap('dev/js/lib/%n.js')
+    file target => ['dev/js/lib', src] do |t|
+        cp(src, t.name)
+    end
+    task :devel => target
+end
+
+FileList['src/coffee/*.coffee'].each do |src|
+    target = src.pathmap('dev/js/gen/%n.js')
+    file target => src do |t|
+        sh "coffee -o #{File.dirname(t.name)} -c #{src}"
+    end
+    CLOBBER.include(target)
+    task :default => target
+end
+        
+
+FileList['src/sass/*.scss'].each do |src|
+    target = src.pathmap('dev/css/%n.css')
+    file target => src do
+        sh "compass compile . #{src}"
+    end
+
+    CLOBBER.include(target)
+    task :default => target
+end
+
 
 if false then
     dep_reqs = FileList['js/lib/*.js']
--- web/config.rb
+++ web/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "dev/css"
+sass_dir = "src/sass"
+images_dir = "dev/img"
+javascripts_dir = "dev/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
